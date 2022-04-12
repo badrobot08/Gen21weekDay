6e0df863c7afa65652e82753cb519044068dc7b2
habbkad <habibkadiri52@gmail.com>
2022-04-12 14:40:38 +0000

    Done with crud edit and delete

--- src/App.js
+++ src/App.js
@@ -6,16 +6,28 @@ import ClassState1 from "./State components/ClassState@";
 import FunctionState from "./State components/FunctionState";
 import "bootstrap/dist/css/bootstrap.min.css";
 import DisplayUsers from "./FormBootstrap/DisplayUsers";
+import PopUpFunctional from "./overView/PopUpFunctional";
+import { v4 as uuidv4 } from "uuid";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <PopUpFunctional />
+//     </div>
+//   );
+// };
 
 function App() {
   const [persons, setPerson] = useState([
     {
       name: "Carl ",
       description: "Likes to Code",
+      id: uuidv4(),
     },
     {
       name: "Andrew",
       description: "likes to party",
+      id: uuidv4(),
     },
   ]);
 
@@ -23,14 +35,32 @@ function App() {
     const newPerson = {
       name: userName,
       description: userDescription,
+      id: uuidv4(),
     };
     setPerson([...persons, newPerson]);
   };
+
+  const deletePerson = (id) => {
+    const newPerson = persons.filter((person) => person.id !== id);
+    setPerson(newPerson);
+  };
+
+  // handle the editing of elements in the array
+  const editPerson = (id, editedData) => {
+    setPerson(
+      persons.map((person) => (person.id === id ? editedData : person))
+    );
+  };
+
   console.log(persons);
   return (
     <>
       <FormHook receiveState={updateState} />
-      <DisplayUsers sendState={persons} />
+      <DisplayUsers
+        sendState={persons}
+        delete={deletePerson}
+        edit={editPerson}
+      />
     </>
   );
 }
